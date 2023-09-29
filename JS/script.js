0a1bd279c3d560c0429b4870e91fcbcb68fceed8
GiacWebDev <giacobechini@gmail.com>
2023-09-29 17:43:42 +0200

    [edit] shift task into tasks-box

--- JS/script.js
+++ JS/script.js
@@ -5,17 +5,23 @@ createApp ({
 
   data() {
     return {
+
       tasks: [
         'fare la pizza',
         'disegnare un quadro',
         'portare fuori il cane'
-      ]
+      ],
+
+      newTask: '',
+      done: false
     }
   },
 
   methods: {
     addTask() {
-      
+      this.tasks.unshift(this.newTask);
+      // quando inserisco la nuova task si elimina il placeholder
+      this.newTask = ''
     }
   }
 
